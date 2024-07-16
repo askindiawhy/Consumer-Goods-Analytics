f543ba4deb2cb897bea8ccc9b882658e4af51cba
Shivam <121374921+askindiawhy@users.noreply.github.com>
2024-07-16 14:19:47 +0530

    Create Code 07.sql

---
+++ Codes/Code 07.sql
@@ -0,0 +1,31 @@
+-- 7. Get the complete report of the Gross sales amount for the customer “Atliq
+-- Exclusive” for each month. This analysis helps to get an idea of low and
+-- high-performing months and take strategic decisions.
+-- The final report contains these columns:
+-- Month
+-- Year
+-- Gross sales Amount
+
+
+Select 
+       Month(date) as Month,
+       Year(date) as Year,
+	   sum(gross_price_total) as total_gross_sales_amount
+       
+from gross_sales
+where customer= "AtliQ Exclusive"
+GROUP BY 
+    YEAR(date), MONTH(date) 
+order by 
+        YEAR(date), MONTH(date); 
+
+----- New Query 
+
+SELECT CONCAT(MONTHNAME(FS.date), ' (', YEAR(FS.date), ')') AS 'Month', FS.fiscal_year,
+       ROUND(SUM(G.gross_price*FS.sold_quantity), 2) AS Gross_sales_Amount
+FROM fact_sales_monthly FS JOIN dim_customer C ON FS.customer_code = C.customer_code
+						   JOIN fact_gross_price G ON FS.product_code = G.product_code
+WHERE C.customer = 'Atliq Exclusive'
+GROUP BY  Month, FS.fiscal_year 
+ORDER BY FS.fiscal_year ;       
+       
