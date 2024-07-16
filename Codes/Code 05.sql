da59340995b68f0c896e7344f4026a60b993c8d2
Shivam <121374921+askindiawhy@users.noreply.github.com>
2024-07-16 14:18:24 +0530

    Create Code 05.sql

---
+++ Codes/Code 05.sql
@@ -0,0 +1,16 @@
+-- Get the products that have the highest and lowest manufacturing costs.
+-- The final output should contain these fields,
+-- product_code
+-- product
+-- manufacturing_cost
+
+
+SELECT m.product_code, concat(product," (",variant,")") AS product, cost_year,manufacturing_cost
+FROM fact_manufacturing_cost m
+JOIN dim_product p ON m.product_code = p.product_code
+WHERE manufacturing_cost= 
+(SELECT min(manufacturing_cost) FROM fact_manufacturing_cost)
+or 
+manufacturing_cost = 
+(SELECT max(manufacturing_cost) FROM fact_manufacturing_cost) 
+ORDER BY manufacturing_cost DESC;
